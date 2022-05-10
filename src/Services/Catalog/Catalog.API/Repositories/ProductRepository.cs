9911acb400fe5233ee8fc5666d19850556388356
diaaelsherif <diaa.elsherif@msn.com>
2022-05-10 19:06:20 +0200

    implemented business layer

---
+++ src/Services/Catalog/Catalog.API/Repositories/ProductRepository.cs
@@ -0,0 +1,54 @@
+﻿using Catalog.API.Data;
+using Catalog.API.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.API.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly ICatalogContext _context;
+
+        public ProductRepository(ICatalogContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        //CRUD Operations in Business Layer
+        public async Task CreateProduct(Product product)
+        {
+            await _context.Products.InsertOneAsync(product);
+        }
+
+        public async Task<Product> GetProduct(string id)
+        {
+            return await _context.Products.Find(p => p.Id == id).FirstOrDefaultAsync();
+        }
+
+        public async Task<Product> GetProductByCategory(string category)
+        {
+            return await _context.Products.Find(p => p.Category == category).FirstOrDefaultAsync();
+        }
+
+        public async Task<Product> GetProductByName(string name)
+        {
+            return await _context.Products.Find(p => p.Name == name).FirstOrDefaultAsync();
+        }
+
+        public async Task<IEnumerable<Product>> GetProducts()
+        {
+            return await _context.Products.Find(p => true).ToListAsync();
+        }
+
+        public async Task<bool> UpdateProduct(Product product)
+        {
+            var updateResult = await _context.Products.ReplaceOneAsync(filter: p => p.Id == product.Id, replacement: product);
+            return updateResult.IsAcknowledged && updateResult.MatchedCount > 0;
+        }
+
+        public async Task<bool> DeleteProduct(string id)
+        {
+            var deleteResult = await _context.Products.DeleteOneAsync(filter: p => p.Id == id);
+            return deleteResult.IsAcknowledged && deleteResult.DeletedCount > 0;
+        }
+    }
+}
